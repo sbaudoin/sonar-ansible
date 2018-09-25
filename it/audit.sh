8d6c064c1ae90436e996ec2f59a353d02c204da6
sbaudoin <sylvain.baudoin@gmail.com>
2018-09-25 07:04:15 +0200

    Integration tests

---
+++ it/audit.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -e
+
+# Create quality profile to enable Ansible rules
+echo "Enabling Ansible rules"
+curl -s -o /dev/null -w "%{http_code}\n" -u admin:admin -X POST 'http://sonarqube:9000/api/qualityprofiles/create?name=Ansible&language=yaml' | grep -q 200
+curl -s -o /dev/null -w "%{http_code}\n" -u admin:admin -X POST 'http://sonarqube:9000/api/qualityprofiles/set_default?qualityProfile=Ansible&language=yaml' | grep -q 204
+PROFILE_KEY=`curl -s -u admin:admin 'http://sonarqube:9000/api/qualityprofiles/search?qualityProfile=Ansible&language=yaml' | sed 's/.*"key":"\([^"]*\)".*/\1/'`
+curl -s -o /dev/null -w "%{http_code}\n" -u admin:admin -X POST 'http://sonarqube:9000/api/qualityprofiles/activate_rules?targetKey='$PROFILE_KEY'&tags=ansible' | grep -q 200
+
+# Install sonar-runner
+echo "Installing Sonar scanner"
+cd /tmp
+wget -q https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SCANNER_VERSION-linux.zip
+unzip -q sonar-scanner-cli-$SCANNER_VERSION-linux.zip
+export PATH=/tmp/sonar-scanner-$SCANNER_VERSION-linux/bin:$PATH
+
+# Configure sonar-runner
+echo "sonar.host.url=http://sonarqube:9000" > /tmp/sonar-scanner-$SCANNER_VERSION-linux/conf/sonar-scanner.properties
+
+# Audit code
+echo "Launching scanner"
+cd /usr/src/myapp/it
+sonar-scanner
+if [ $? -ne 0 ]
+then
+    echo "Error scanning Ansible playbooks" >&2
+    exit 1
+fi
+
+# Check audit result
+echo "Checking result"
+apt-get update
+apt-get install -y python-pip
+pip install requests
+python << EOF
+import requests
+import sys
+
+r = requests.get('http://sonarqube:9000/api/measures/component?component=my:project&metricKeys=ncloc,comment_lines,lines,files,directories,violations', auth=('admin', 'admin'))
+if r.status_code != 200:
+    sys.exit(1)
+
+data = r.json()
+
+if 'component' not in data or 'measures' not in data['component']:
+    sys.exit(1)
+
+lines = ncloc = files = directories = comment_lines = False
+for measure in data['component']['measures']:
+    if measure['metric'] == 'lines' and measure['value'] == '127':
+        print 'lines metrics OK'
+        lines = True
+    if measure['metric'] == 'ncloc' and measure['value'] == '87':
+        print 'ncloc metrics OK'
+        ncloc = True
+    if measure['metric'] == 'files' and measure['value'] == '12':
+        print 'files metrics OK'
+        files = True
+    if measure['metric'] == 'directories' and measure['value'] == '7':
+        print 'directories metrics OK'
+        directories = True
+    if measure['metric'] == 'comment_lines' and measure['value'] == '1':
+        print 'comment_lines metrics OK'
+        comment_lines = True
+    if measure['metric'] == 'violations' and measure['value'] == '27':
+        print 'violations metrics OK'
+        comment_lines = True
+
+r = requests.get('http://sonarqube:9000/api/issues/search?componentKeys=my:project:src/roles/bobbins/tasks/main.yml&statuses=OPEN', auth=('admin', 'admin'))
+if r.status_code != 200:
+    sys.exit(1)
+
+data = r.json()
+
+if data['total'] != 1:
+    sys.exit(1)
+issues = False
+if data['issues'][0]['message'] == 'Git checkouts must contain explicit version' and data['issues'][0]['line'] == 2:
+    print 'issues metrics OK'
+    issues = True
+
+sys.exit(0 if lines and ncloc and files and directories and comment_lines and issues else 1)
+EOF
---
+++ it/it.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+export SONARQUBE_VERSION="$1"
+export SCANNER_VERSION="$2"
+if [ -z "$SCANNER_VERSION" ]
+then
+    echo "Missing parameters: <SonarQube version> <scanner version>" >&2
+    exit 1
+fi
+
+export SCRIPT_DIR=`dirname $0`
+
+# Clean-up if needed
+echo "Cleanup..."
+docker-compose -f $SCRIPT_DIR/docker-compose.yml down
+
+# Start containers
+echo "Starting SonarQube..."
+#CONTAINER_NAME=`docker-compose -f $SCRIPT_DIR/docker-compose.yml up -d sonarqube 2>&1 | grep -o '[^ ]*sonarqube[^ ]*' | head -1`
+docker-compose -f $SCRIPT_DIR/docker-compose.yml up -d sonarqube
+CONTAINER_NAME=it_sonarqube_1
+# Wait for SonarQube to be up
+grep -q "SonarQube is up" <(docker logs --follow --tail 0 $CONTAINER_NAME)
+# Copy the plugins
+sudo pip install lxml
+YAML_PLUGIN_VERSION=$(python -c "
+from lxml import etree
+
+pom = etree.parse('/mnt/pom.xml')
+print pom.xpath('/a:project/a:dependencyManagement//a:dependency[a:artifactId=\'sonar-yaml-plugin\']/a:version', namespaces={'a': 'http://maven.apache.org/POM/4.0.0'})[0].text
+")
+wget -O /tmp/sonar-yaml-plugin-$YAML_PLUGIN_VERSION.jar https://oss.sonatype.org/content/groups/public/com/github/sbaudoin/sonar-yaml-plugin/$YAML_PLUGIN_VERSION/sonar-yaml-plugin-$YAML_PLUGIN_VERSION.jar
+docker cp /tmp/sonar-yaml-plugin-$YAML_PLUGIN_VERSION.jar $CONTAINER_NAME:/opt/sonarqube/extensions/plugins
+docker cp $SCRIPT_DIR/../sonar-ansible-plugin/target/sonar-ansible-plugin-*.jar $CONTAINER_NAME:/opt/sonarqube/extensions/plugins
+# Restart SonarQube
+docker-compose -f $SCRIPT_DIR/docker-compose.yml restart sonarqube
+# Wait for SonarQube to be up
+grep -q "SonarQube is up" <(docker logs --follow --tail 0 $CONTAINER_NAME)
+# Check plug-in installation
+if ! docker exec $CONTAINER_NAME curl -su admin:admin http://localhost:9000/api/plugins/installed | python -c '
+import sys
+import json
+
+data = json.loads(sys.stdin.read())
+if "plugins" in data:
+    for plugin in data["plugins"]:
+        if plugin["key"] == "ansible":
+            sys.exit(0)
+sys.exit(1)
+'
+then
+    echo "Plugin not installed" >&2
+    exit 1
+fi
+
+# Audit code
+echo "Audit Ansible test playbooks..."
+docker-compose -f $SCRIPT_DIR/docker-compose.yml up --build --exit-code-from auditor auditor
+AUDIT_STATUS=$?
+
+# Delete containers
+echo "Cleanup..."
+docker-compose -f $SCRIPT_DIR/docker-compose.yml down
+
+exit $AUDIT_STATUS
